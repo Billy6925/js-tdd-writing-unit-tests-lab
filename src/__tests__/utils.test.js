5965f9720d40e0b601539777965ebd5f3344b1c3
Billy Eskein <billyeskein@gmail.com>
2025-02-08 18:45:40 +0300

    Update src/utils.js

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,15 @@
 // Your tests here
+import {isPalindrome} from '../utils';
+
+describe('isPalindrome', () => {
+    it('returns true if the string is a palindrome', () => {
+    const word = 'racecar';
+    const notWord = 'car';
+    const palindrome = isPalindrome(word);
+    const notPalindrome = isPalindrome(notWord);
+    expect(palindrome).toBe(true);
+    expect(notPalindrome).toBe(false);
+    });
+});
+
+
