c601efe20d6d53700cc89249a1dad114b8ae5d1c
Billy Eskein <billyeskein@gmail.com>
2025-02-08 20:50:28 +0300

    Update src/__tests__/utils.test.js

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,15 +1,17 @@
-// Your tests here
-import {isPalindrome} from '../utils';
+import { isPalindrome } from '../utils';
 
 describe('isPalindrome', () => {
     it('returns true if the string is a palindrome', () => {
-    const word = 'RaCeCaR';
-    const notWord = '';
-    const palindrome = isPalindrome(word);
-    const notPalindrome = isPalindrome(notWord);
-    expect(palindrome).toBe(true);
-    expect(notPalindrome).toBe(false);
+        const word = 'RaCeCaR';
+        const notWord = '';
+        const palindrome = isPalindrome(word);
+        const notPalindrome = isPalindrome(notWord);
+        expect(palindrome).toBe(true);
+        expect(notPalindrome).toBe(false);
+    });
+
+    it('throws an error if the input contains non-alphabetic characters', () => {
+        expect(() => isPalindrome('RaCeCaR123')).toThrow('Input contains non-alphabetic characters');
+        expect(() => isPalindrome('!racecar!')).toThrow('Input contains non-alphabetic characters');
     });
 });
-
-
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,9 @@
 // Your code here
 export function isPalindrome(word) {
     if(!word) return false;
+    if(!/^[A-Za-z]+$/.test(word)){
+        throw new Error("Input contains non-alphabetic characters");
+    }
     const reversedWord = word.toLowerCase().split('').reverse().join('');
     return word.toLowerCase() === reversedWord;
 }
