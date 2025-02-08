d508a66bd6daaf440f342d1f17e55d85e143ec80
Billy Eskein <billyeskein@gmail.com>
2025-02-08 21:14:13 +0300

    Update src/__tests__/utils.test.js

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,17 +1,31 @@
-import { isPalindrome } from '../utils';
+import {isPalindrome} from '../utils';
 
-describe('isPalindrome', () => {
-    it('returns true if the string is a palindrome', () => {
-        const word = 'RaCeCaR';
-        const notWord = '';
-        const palindrome = isPalindrome(word);
-        const notPalindrome = isPalindrome(notWord);
+describe('isPalindrome',()=> {
+    it('return true if a string is a palindrome',()=>{
+        //Arrange
+        const word = 'RaCeCaR'
+        //Act
+        const palindrome = isPalindrome(word)
+        //Assert
         expect(palindrome).toBe(true);
+    })
+    it('returns false for an empty string',()=>{
+        //Arrange
+        const notWord = ''
+        //Act
+        const notPalindrome = isPalindrome(notWord)
+        //Assert
         expect(notPalindrome).toBe(false);
-    });
-
-    it('throws an error if the input contains non-alphabetic characters', () => {
-        expect(() => isPalindrome('RaCeCaR123')).toThrow('Input contains non-alphabetic characters');
-        expect(() => isPalindrome('!racecar!')).toThrow('Input contains non-alphabetic characters');
-    });
-});
+    })
+    it('throws an error if the input contains non-alphabetic characters',()=>{
+        expect(() =>isPalindrome('RaCeCaR123')).toThrow('Input contains non-alphabetic characters')
+expect(()=>isPalindrome('!racecar!')).toThrow('Input contains non-alphabetic characters')
+    })
+    it('throws an error if the input is not a string',()=>{
+        expect(() =>isPalindrome(123)).toThrow('Input is not a string')
+        expect(()=> isPalindrome(null).toThrow('Input is not a string'))
+        expect(()=>isPalindrome(undefined).toThrow('Input is not a string'))
+        expect(()=>isPalindrome({}).toThrow('Input is not a string'))
+        expect(()=>isPalindrome([]).toThrow('Input is not a string'))
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,6 @@
 // Your code here
 export function isPalindrome(word) {
+    if(typeof word !== 'string') throw new Error("Input is not a string");
     if(!word) return false;
     if(!/^[A-Za-z]+$/.test(word)){
         throw new Error("Input contains non-alphabetic characters");
